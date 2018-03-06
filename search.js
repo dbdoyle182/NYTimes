a1245ef38b3eeaff3a4c84992ba0e67f349c1c58
Andrew <jaknowles1997@gmail.com>
2018-03-06 10:48:10 -0800

    intial js setup

---
+++ search.js
@@ -0,0 +1,24 @@
+var page = 0;
+
+$("#searchBtn").on("click", function(event) {
+
+    // Here we grab the text from the input boxes
+    var search = $("#searchTerm").val();
+    var limit = $("#recordLimit").val();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
+
+    // Here we construct our URL
+    var apiKey =  "?api-key=5eca8e97a59c428ea2e628d8e052c3eb";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+
+         search + apiKey; //+"?page="+ page+ "?begin_date=" +startYear+ "?end_date=" + endYear + apiKey;
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(res) {
+    console.log(res);
+    $("#articleResults").html(JSON.stringify(res));
+    });
+
+});
