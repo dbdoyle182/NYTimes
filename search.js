546f1f22de22e6ecad50f33c007d67a849727a30
Andrew <jaknowles1997@gmail.com>
2018-03-07 05:48:56 -0800

    finally, working logic

--- search.js
+++ search.js
@@ -1,25 +1,56 @@
-var page = 0;
+var fetchData = function(limit, queryURL) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(data) {
+        for(var i = 0; i < limit; i++) {
+            if(data.response.docs[i].headline.main != null)
+            var title = data.response.docs[i].headline.main;
+            if(data.response.docs[i].byline.original != null)
+            var author = data.response.docs[i].byline.original;
+            if(data.response.docs[i].pub_date != null)
+            var publish = data.response.docs[i].pub_date;
+            if(data.response.docs[i].web_url != null)
+            var source = data.response.docs[i].web_url;
 
-$("#searchBtn").on("click", function(event) {
+            articleHtml =
+            "<h3>" + title + "</h3>"+
+            "<h5>" + author + "</h5>"+
+            "<p>" + publish + "</p>" +
+            "<a href="+ source +"'>" + source + "</a>";
+
+            var  articleItem = $("<div>").html(articleHtml);
+            $("#articleResults").append(articleItem);
+
+        }
+    });
+}
+
+$("#searchBtn").on("click", function() {
 
     // Here we grab the text from the input boxes
-    var search = $("#searchTerm").val();
+    var search = $("#searchTerm").val().trim();
     var limit = $("#recordLimit").val();
-    var startYear = $("#startYear").val();
-    var endYear = $("#endYear").val();
+    var startYear = $("#startYear").val().trim();
+    var endYear =  $("#endYear").val().trim();
 
-    // Here we construct our URL
+    // Our bare bones queryURL
     var apiKey =  "&api-key=5eca8e97a59c428ea2e628d8e052c3eb";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+
-         search +"?page="+ page+ "?begin_date=" +startYear+ "?end_date=" + endYear + apiKey;
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ search + apiKey;
 
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(res) {
-    console.log(res);
-    var article = $("<li>").text(res.response.docs[0].snippet)
-    $("#articleResults").append(article);
-    });
+    if(parseInt(startYear)) {
+        queryURL += "&begin_date=" + startYear + "0101";
 
+    }
+    if(parseInt(endYear)) {
+        queryURL += "&end_date=" + endYear + "0101";
+    }
+
+    fetchData(limit,queryURL);
+    console.log(queryURL);
+
+});
+
+$("#clearResults").on("click", function() {
+    $("#articleResults").empty();
 });
